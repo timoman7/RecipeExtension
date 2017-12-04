f001294e4e38a57f1916ebedd642d1bbf500ac40
Tim C <timoman8@hotmail.com>
2017-12-04 14:58:19 -0500

    From laptop

---
+++ grabFromGithub.js
@@ -0,0 +1,36 @@
+var abc;
+var filePath = "https://raw.githubusercontent.com/timoman7/RecipeExtension/master/";
+var files = {};
+
+function recursiveJSON(frag, cb) {
+  for (let _frag in frag) {
+    if (typeof frag[_frag] == "object") {
+      recursiveJSON(frag[_frag], cb, _frag)
+    } else {
+      if (arguments[2]) {
+        cb(arguments[2], frag[_frag]);
+      } else {
+        cb(frag[_frag]);
+      }
+    }
+  }
+}
+fetch(filePath + "filepaths.json").then((result) => {
+  return result.json();
+}).then((json) => {
+  abc = json;
+  recursiveJSON(json, function() {
+    var arg = arguments;
+    fetch(filePath + (([...arguments].join("")).replace(/ /g, ""))).then((result) => {
+      if (arg[0].endsWith("/")) {
+        if (!files[arg[0]]) {
+          files[arg[0]] = {};
+        } else {
+          files[arg[0]][arg[1]] = result;
+        }
+      } else {
+        files[arg[0]] = result;
+      }
+    });
+  })
+});
